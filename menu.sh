ffbdba80369dd3de9e3b2b4e2f60a45d3e050e66
LoveArora01 <arora337@gmail.com>
2021-04-11 03:11:53 +0000

    Code change in menu card

--- menu.sh
+++ menu.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-.
-.
-# This is a demo script for creating menu option.
-.
-.
 # We'll use echo command to take input by user and save into mymenu.sh.
 
-echo -e "\t\t\t!!!! SHREE BHOJNALYA !!!!\t\t\t" > mymenu.sh
+echo -e "\t\t\t!!!! SHREE BHOJNALYA !!!!\t\t\t" > mymenu
+i=1
+while [ "$i" -le 5 ]
+do
 echo -e "Order Name: \c" ; read Order
-echo "$Order" >> mymenu.sh
+echo -e "Order name: $Order" >> mymenu
+i=$((i+1))
+done
+
+
